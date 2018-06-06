794fcdc5bbc88e52f4f9a98af320d45e08ce82b9
sandeepsandy <sandeep.mulkala@globalnest.com>
2018-06-06 15:20:59 +0530

    sandeep0506:company profile design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BasicInfoComponent } from './basic-info/basic-info.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { CompanyProfileComponent } from './company-profile/company-profile.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicInfoComponent
+    BasicInfoComponent,
+    CompanyProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -20,4 +22,5 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/company-profile/company-profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+declare var $ :any;
+
+@Component({
+  selector: 'app-company-profile',
+  templateUrl: './company-profile.component.html',
+  styleUrls: ['./company-profile.component.css']
+})
+export class CompanyProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
